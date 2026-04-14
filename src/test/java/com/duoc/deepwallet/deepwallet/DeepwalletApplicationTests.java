14a64c97527397f27a7f4a8018078651df0166ba
Bastian Huenchullan <ba.huenchullan@duocuc.cl>
2026-04-14 13:50:42 -0400

    v1 - SpringInitializr Base

---
+++ src/test/java/com/duoc/deepwallet/deepwallet/DeepwalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.deepwallet.deepwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepwalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
